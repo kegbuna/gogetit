db46ab3bff66de623663a47d05da7da65b4a04ff
Ken Egbuna <kegbuna@gmail.com>
2016-09-26 02:59:04 -0400

    Initial Commit

---
+++ config/urls.js
@@ -0,0 +1,2 @@
+export const PRESS_RELEASES = 'https://www.sec.gov/rss/news/press.xml';
+export const SPEECHES = 'https://www.sec.gov/rss/news/speech.xml';
---
+++ index.js
@@ -0,0 +1,43 @@
+import restify from 'restify';
+import request from 'request';
+import FeedMe from 'feedme';
+import Logger from 'log4js';
+
+import { PRESS_RELEASES, SPEECHES } from './config/urls';
+
+const logger = Logger.getLogger('Index');
+
+function get(url) {
+  return (req, res, next) => {
+    const parser = new FeedMe(true);
+
+    logger.debug(`Retrieving from ${url}.`);
+    request(url)
+      .pipe(parser)
+      .on('error', err => {
+        logger.error(err);
+      });
+
+    parser.on('end', data => {
+      const results = parser.done();
+      res.send(results);
+    })
+  }
+}
+
+const server = restify.createServer();
+server.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+);
+
+
+server.get('/pressreleases', get(PRESS_RELEASES));
+server.get('/speeches', get(SPEECHES));
+
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});

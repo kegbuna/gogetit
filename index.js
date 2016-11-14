50bb187329627f97c926b6f5596203a360423be9
Ken Egbuna <kegbuna@gmail.com>
2016-11-14 15:57:28 -0500

    fixing import

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import restify from 'restify';
 import config from './config.json';
-import * as directoryRoutes from './routes/directory';
+import directoryGet from './routes/directory';
 
 const server = restify.createServer();
 server.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    return next();
-  }
-);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  return next();
+});
 
-server.get('/govreader/directory', directoryRoutes.Get);
-server.get('/govreader/directory/:department', directoryRoutes.Get);
-server.get('/govreader/directory/:department/:feedIndex', directoryRoutes.Get);
+server.get('/govreader/directory', directoryGet);
+server.get('/govreader/directory/:department', directoryGet);
+server.get('/govreader/directory/:department/:feedIndex', directoryGet);
 
 server.listen(config.port, () => {
   console.log('%s listening at %s', server.name, server.url);

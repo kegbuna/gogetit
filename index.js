6db2fa27f7315af526481f9c8f293aae9c4cf8ba
Ken Egbuna <kegbuna@gmail.com>
2016-11-15 06:09:59 -0500

    merge conflict, logger

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import restify from 'restify';
+import Logger from 'log4js';
 import config from './config.json';
 import directoryGet from './routes/directory';
 
+const logger = Logger.getLogger('Index');
+
 const server = restify.createServer();
-server.use((req,res,next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   return next();
 });
 
@@ -14,5 +17,5 @@ server.get('/govreader/directory/:department', directoryGet);
 server.get('/govreader/directory/:department/:feedIndex', directoryGet);
 
 server.listen(config.port, () => {
-  console.log('%s listening at %s', server.name, server.url);
+  logger.info('%s listening at %s', server.name, server.url);
 });
